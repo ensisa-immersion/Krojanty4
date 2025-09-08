cdbf386ca9b17be9a335f61614ce2ac610cee9d6
georges.el-chater@uha.fr <georges.el-chater@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-08 19:10:25 +0000

    Nouveau display.h fonctionnel
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@28 35732415-9901-0010-945d-51890b626cc3

---
+++ include/display.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAY_H_INCLUDED
+#define DISPLAY_H_INCLUDED
+
+#include <gtk/gtk.h>
+#include "game.h"
+
+#define GRID_SIZE 9
+#define CELL_SIZE 40
+
+void draw_callback(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data);
+void activate (GtkApplication *app, gpointer user_data);
+int initialize_display(int argc, char** argv, Game* game);
+
+#endif // DISPLAY_H_INCLUDED
