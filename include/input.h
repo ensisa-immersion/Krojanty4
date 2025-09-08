8c0bcefedd1612655ceb20277f224e1f07012362
georges.el-chater@uha.fr <georges.el-chater@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-08 19:10:02 +0000

    Je debute le fichier input
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@27 35732415-9901-0010-945d-51890b626cc3

---
+++ include/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H_INCLUDED
+#define INPUT_H_INCLUDED
+
+#include "game.h"
+
+static void on_click(GtkGestureClick *gesture,
+                     int n_press,
+                     double x, double y,
+                     gpointer user_data);
+
+void detect_click(GtkWidget *drawing_area, Game *game);
+
+#endif // INPUT_H_INCLUDED
