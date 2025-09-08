cf6b88d6ffbb31d5ed7ef87a7eb582af9857947b
loic.maurer@uha.fr <loic.maurer@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-08 17:46:16 +0000

    Moove files between differents folders srv -> include
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@20 35732415-9901-0010-945d-51890b626cc3

---
+++ include/server.h
@@ -0,0 +1,19 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <netinet/in.h>
+
+#define PORT_SERVEUR 5555
+#define TAILLE_BUFFER 64
+
+void initialiser_adresse(struct sockaddr_in *adresse, int port);
+int initialiser_socket(struct sockaddr_in *adresse, int max_conn);
+int accepter_client(int sock);
+int coord_valide(char col, char row);
+int entree_valide(const char *buffer);
+int deplacement_valide(const char *buffer);
+void envoyer_message_client(int fd, const char *msg);
+int lire_message_client(int fd, char *buffer, int max_size);
+int lire_entree_serveur(char *buffer, int max_size);
+
+#endif // SERVER_H
