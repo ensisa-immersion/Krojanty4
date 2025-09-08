f9929f8ea5132b3f3fed8f64c3e867eb6b8f268b
anna.mauroy@uha.fr <anna.mauroy@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-08 15:38:36 +0000

    feat: beaucoup de logique dans game.c
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@18 35732415-9901-0010-945d-51890b626cc3

--- include/game.h
+++ include/game.h
@@ -1,8 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
 
+struct user_move_s {
+    int delta_x; // changement en x (ligne)
+    int delta_y; // changement en y (colonne)
+};
+typedef struct user_move_s user_move;
+
 int coord_valide(char col, char row);
 int entree_valide(const char *buffer);
-int deplacement_diagonale_interdit(const char *buffer);
+user_move parse_move(const char *buffer);
+int deplacement_diagonale_interdit(user_move move);
 
 #endif //CLIENT_H
