44fe95c84eca3e46bb9bc1e2334a8e9886d9029f
loic.maurer@uha.fr <loic.maurer@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-09 12:10:35 +0000

    Add -s argument to host a server
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@36 35732415-9901-0010-945d-51890b626cc3

--- main.c
+++ main.c
@@ -8,51 +8,69 @@
 #include "include/game.h"
 
 /**
- * Lance le code principal du serveur.
+ * Lance le code principal du serveur ou du client.
  * @param argc Nombre d'arguments de la ligne de commande.
  * @param argv Tableau des arguments de la ligne de commande.
  */
 int main(int argc, char *argv[])
 {
     // Vérification des arguments
-    if (argc != 3 || strcmp(argv[1], "-c") != 0)
+    if (argc != 3 || (strcmp(argv[1], "-c") != 0 && strcmp(argv[1], "-s") != 0))
     {
-        printf("Usage : %s -c <adresse_ip>:<port>\n", argv[0]);
-        printf("Exemple : %s -c 127.0.0.1:8080\n", argv[0]);
-        return 1;
-    }
-
-    // Récupération de la chaîne ip:port
-    char *ip_port = argv[2];
-    char *delimiter = strchr(ip_port, ':');
-
-    if (delimiter == NULL)
-    {
-        printf("Format incorrect. Utilisez <adresse_ip>:<port>\n");
-        return 1;
-    }
-
-    // Extraction de l'adresse IP et du port
-    *delimiter = '\0';
-    const char *ip_address = ip_port;
-    int port = atoi(delimiter + 1);
-
-    // Vérification de la validité du port
-    if (port <= 0 || port > 65535)
-    {
-        printf("Port invalide. Utilisez un port entre 1 et 65535.\n");
+        printf("Usage : %s -c <adresse_ip>:<port> | -s <port>\n", argv[0]);
+        printf("Exemple client : %s -c 127.0.0.1:8080\n", argv[0]);
+        printf("Exemple serveur : %s -s 8080\n", argv[0]);
         return 1;
     }
 
     Game game = init_game();               // Initializes game
     initialize_display(argc, argv, &game); // Initializes display and also initializes click listener
 
-    // Démarrage du client
-    printf("Démarrage du client...\n");
-    int socket = client("10.171.251.235", 5555);
+    if (strcmp(argv[1], "-c") == 0)
+    {
+        // Mode client
+        char *ip_port = argv[2];
+        char *delimiter = strchr(ip_port, ':');
 
-    // Envoyer un message en utilisant la fonction send_message du fichier client.c
-    send_message(socket, "B2:A2");
+        if (delimiter == NULL)
+        {
+            printf("Format incorrect. Utilisez <adresse_ip>:<port>\n");
+            return 1;
+        }
+
+        *delimiter = '\0';
+        const char *ip_address = ip_port;
+        int port = atoi(delimiter + 1);
+
+        if (port <= 0 || port > 65535)
+        {
+            printf("Port invalide. Utilisez un port entre 1 et 65535.\n");
+            return 1;
+        }
+
+        printf("Démarrage du client...\n");
+        int socket = client(ip_address, port);
+
+        send_message(socket, "B2:A2");
+    }
+    else if (strcmp(argv[1], "-s") == 0)
+    {
+        // Mode serveur
+        int port = atoi(argv[2]);
+        if (port <= 0 || port > 65535)
+        {
+            printf("Port invalide. Utilisez un port entre 1 et 65535.\n");
+            return 1;
+        }
+
+        printf("Démarrage du serveur sur le port %d...\n", port);
+        int server_socket = start_server(port); // start_server à implémenter dans server.c
+
+        // Connexion immédiate du client local
+        int client_socket = client("127.0.0.1", port);
+
+        send_message(client_socket, "B2:A2");
+    }
 
     return 0;
 }
