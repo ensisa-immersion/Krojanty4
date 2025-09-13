0cf93df178d27b0395348e56ed001cf2edba46d8
georges.el-chater@uha.fr <georges.el-chater@uha.fr@35732415-9901-0010-945d-51890b626cc3>
2025-09-13 21:15:52 +0000

    Ajout algo h
    git-svn-id: https://svn.ensisa.uha.fr/scm/svn/imm2526-gr4@98 35732415-9901-0010-945d-51890b626cc3

---
+++ include/algo.h
@@ -0,0 +1,31 @@
+#ifndef ALGO_H_INCLUDED
+#define ALGO_H_INCLUDED
+
+#include "game.h"
+
+typedef struct {
+    int src_row;
+    int src_col;
+    int dst_row;
+    int dst_col;
+    int score;
+} Move;
+
+typedef struct {
+    Move s_move;
+    int score;
+} ScoredMove;
+
+// Essential functions that describe game state
+int utility(Game * game);
+int all_possible_moves(Game * game, Move * move_list, Player player);
+int all_possible_moves_ordered(Game *game, Move * move_list, Player player);
+
+// AI computing functions
+int minimax_alpha_beta(Game * game, int depth, int maximizing, int alpha, int beta);
+Move minimax_best_move(Game * game, int depth);
+
+// API for game.c
+void ai_next_move(Game* game);
+
+#endif // ALGO_H_INCLUDED
